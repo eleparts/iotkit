4e00811b052347702788a16e0c168fadf0e24b30
eleparts <nyk.yeon@eleparts.co.kr>
2019-07-23 02:30:05 +0100

    blynk GPIO 예제 및 릴레이 추가 예제

--- start.sh
+++ start.sh
@@ -4,3 +4,8 @@ echo "Download example files"
 echo "git clone PMS7003 example -> ./examples/05.PMS7003"
 git clone https://github.com/eleparts/PMS7003 ./examples/05.PMS7003
 
+echo "git clone blynk_python_GPIO example -> ./blynk_examples/blynk_python_GPIO"
+git clone https://github.com/eleparts/blynk_python_GPIO ./blynk_examples/blynk_python_GPIO
+
+echo "cp blynk_python_GPIO_V2.py"
+cp ./blynk_examples/blynk_python_GPIO_V2.py ./blynk_examples/blynk_python_GPIO/blynk_python_GPIO_V2.py
